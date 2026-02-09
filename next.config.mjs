0206583703aa10d2996fc838ca53e086521cc992
Hassan <hassan111.bmw11@gmail.com>
2026-02-09 11:38:25 +0300

    add:basePath in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: "/e-commerceShop",
+  assetPrefix: "/e-commerceShop",
 };
 
 export default nextConfig;
